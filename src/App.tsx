bc53625f345ff95f732ece1dd9d0984d904afd44
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-04-28 12:33:19 +0200

    add CreateNewUser component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CreateNewUser from "./components/CreateNewUser";
 import ListOfUser from "./components/ListOfUser";
 
 function App() {
 	return (
 		<>
 			<ListOfUser />
+			<CreateNewUser />
 		</>
 	);
 }
